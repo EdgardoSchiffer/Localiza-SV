ea41733812f74f7e749a19105fbf7dba76e6cf97
Edgardo Bautista <ed_bautista044@hotmail.com>
2016-04-11 17:59:34 -0600

    Initial commit

---
+++ bin/WEB-INF/resources/js/angularjs/login.js
@@ -0,0 +1,10 @@
+var login = angular.module('loginApp', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+	$mdThemingProvider.theme('default')
+		.primaryPalette('teal')
+		.accentPalette('indigo');
+});
+
+login.run(function(){
+	alert("Gola")
+});
